bbdd377cf452aac011cb1d0fb72d48d87f6f4d99
sagar <sagarpatilte@gmail.com>
2024-12-19 16:39:21 +0530

    quit statement

--- src/main/java/org/sele/git/TestFirst.java
+++ src/main/java/org/sele/git/TestFirst.java
@@ -9,21 +9,21 @@ public class TestFirst {
     @Test
     public void launch1(){
         System.out.println(System.getProperty("user.dir"));
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/org/sele/git/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/org/sele/git/chromedriverlinux");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.javatpoint.com/selenium-webdriver-first-test-case");
 
     }
     @Test
     public void launch2(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriverlinux");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.browserstack.com/docs/automate/selenium/github-actions");
 
     }
     @Test
     public void launch3(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriverlinux");
         WebDriver driver = new ChromeDriver();
         driver.get("https://github.com/features/actions");
 
