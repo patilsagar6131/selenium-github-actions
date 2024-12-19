b5dcd4efc6c001b16f995a473704816206be583c
sagar <sagarpatilte@gmail.com>
2024-12-19 13:26:58 +0530

    quit statement

--- src/main/java/org/sele/git/TestFirst.java
+++ src/main/java/org/sele/git/TestFirst.java
@@ -7,24 +7,27 @@ import org.testng.annotations.Test;
 public class TestFirst {
 
     @Test
-    public void launch1(){
+    public void launch1() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.javatpoint.com/selenium-webdriver-first-test-case");
-
+        Thread.sleep(4000);
+        driver.quit();
     }
     @Test
-    public void launch2(){
+    public void launch2() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.browserstack.com/docs/automate/selenium/github-actions");
-
+        Thread.sleep(4000);
+        driver.quit();
     }
     @Test
-    public void launch3(){
+    public void launch3() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://github.com/features/actions");
-
+        Thread.sleep(4000);
+        driver.quit();
     }
 }
