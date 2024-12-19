deb05d5d157dbad2281d8c6c21adeee6b7c1270c
sagar <sagarpatilte@gmail.com>
2024-12-19 20:27:07 +0530

    quit statement

--- src/main/java/org/sele/git/TestFirst.java
+++ src/main/java/org/sele/git/TestFirst.java
@@ -1,5 +1,6 @@
 package org.sele.git;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,9 +9,10 @@ public class TestFirst {
 
   @Test
     public void launch1(){
-        System.out.println(System.getProperty("user.dir"));
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/org/sele/git/chromedriverlinux");
-        WebDriver driver = new ChromeDriver();
+//        System.out.println(System.getProperty("user.dir"));
+//        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/org/sele/git/chromedriverlinux");
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
         driver.get("https://www.javatpoint.com/selenium-webdriver-first-test-case");
 
     }
