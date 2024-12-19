7ff78583942feb034ba85f95d95c17fada11032f
sagar <sagarpatilte@gmail.com>
2024-12-19 12:56:56 +0530

    first commit to seleniumgithub

---
+++ src/main/java/org/sele/git/TestFirst.java
@@ -0,0 +1,30 @@
+package org.sele.git;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestFirst {
+
+    @Test
+    public void launch1(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.javatpoint.com/selenium-webdriver-first-test-case");
+
+    }
+    @Test
+    public void launch2(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.browserstack.com/docs/automate/selenium/github-actions");
+
+    }
+    @Test
+    public void launch3(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI1\\IdeaProjects\\seleniumGithubActions\\src\\main\\java\\org\\sele\\git\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com/features/actions");
+
+    }
+}
